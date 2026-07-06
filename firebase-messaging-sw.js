7435190d058d17f6e7308c2870de4ab821d7fe6f
Molde-tech <mark.moldenhauer1991@gmail.com>
2026-07-06 10:14:15 +0200

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,8 +1,27 @@
-self.addEventListener('install', (event) => {
-  console.log('SW installiert');
-  self.skipWaiting();
+importScripts('https://www.gstatic.com/firebasejs/10.12.5/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.12.5/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDXzKjeyfGAybqM015oINBPqjLk1TedE68",
+  authDomain: "vfb-westhofen-app.firebaseapp.com",
+  projectId: "vfb-westhofen-app",
+  storageBucket: "vfb-westhofen-app.firebasestorage.app",
+  messagingSenderId: "405760041608",
+  appId: "1:405760041608:web:bd3d572508cede413bf53d"
 });
 
-self.addEventListener('activate', (event) => {
-  console.log('SW aktiviert');
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+
+  const title =
+      payload.notification?.title || 'VFB Westhofen';
+
+  const body =
+      payload.notification?.body || '';
+
+  self.registration.showNotification(title,{
+      body: body
+  });
+
 });
