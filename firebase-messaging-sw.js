83f26fb993cdd20936e50f0e86ea4e1f80af5b6a
Molde-tech <mark.moldenhauer1991@gmail.com>
2026-07-07 10:43:57 +0200

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,12 +2,12 @@ importScripts('https://www.gstatic.com/firebasejs/10.12.5/firebase-app-compat.js
 importScripts('https://www.gstatic.com/firebasejs/10.12.5/firebase-messaging-compat.js');
 
 firebase.initializeApp({
-  apiKey: "AIza...",
+  apiKey: "AIzaSyDXzKjeyfGAybqM015oINBPqjLk1TedE68",
   authDomain: "vfb-westhofen-app.firebaseapp.com",
   projectId: "vfb-westhofen-app",
   storageBucket: "vfb-westhofen-app.firebasestorage.app",
   messagingSenderId: "405760041608",
-  appId: "1:405760041608:web:..."
+  appId: "1:405760041608:web:bd3d572508cede413bf53d"
 });
 
 const messaging = firebase.messaging();
